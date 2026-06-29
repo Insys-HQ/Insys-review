c3781e969293df059e21bf40181ddcb119ed3ee1
Insys <ys.kim@in-sys.co.kr>
2026-06-29 14:08:31 +0900

    add firebase-config.js v1.0

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+/**
+ * 인시스 그룹 공용 Firebase 설정
+ * 인시스 AI혁신 R&D센터
+ * 프로젝트: insys-work
+ * v1.0 | 2026-06-29
+ *
+ * ★ 이 파일 하나만 관리하면 이후 만들어지는 모든 앱에 자동 적용됩니다.
+ * ★ 사용법: HTML <script type="module"> 안에서
+ *   import { INSYS_FIREBASE_CONFIG } from './firebase-config.js';
+ *   initializeApp(INSYS_FIREBASE_CONFIG);
+ */
+
+export const INSYS_FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyBYqd2uRr1ZvFLDCWuS15h9qFkT-Dvg5Es",
+  authDomain:        "insys-work.firebaseapp.com",
+  projectId:         "insys-work",
+  storageBucket:     "insys-work.firebasestorage.app",
+  messagingSenderId: "225275784859",
+  appId:             "1:225275784859:web:bd12815c6dc47c0c49e03e"
+};
